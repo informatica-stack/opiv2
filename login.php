a121d76e3cc6153f432c2b2a1b9ada28690885db
informatica-stack <informatica@lebu.cl>
2026-08-05 18:01:04 -0400

    upd

--- login.php
+++ login.php
@@ -227,7 +227,7 @@ function redirectBasedOnRole($role) {
             </div>
 
             <!-- BOTÓN OFICIAL CLAVEÚNICA (Ejemplo Institucional Lebu) -->
-            <a class="btn-cu btn-m btn-color-estandar shadow-sm" href="<?= obtener_url_claveunica() ?>" aria-label="Iniciar sesión con ClaveÚnica">
+            <a class="btn-cu btn-m btn-color-estandar btn-fw rounded-middle shadow-sm" href="<?= obtener_url_claveunica() ?>" aria-label="Iniciar sesión con ClaveÚnica">
                 <svg class="cl-claveunica-svg" width="24" height="24" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path d="M12.4998 13.8956C12.9835 13.8956 13.3756 14.2878 13.3756 14.7715C13.3756 15.2552 12.9835 15.6473 12.4998 15.6473C12.0161 15.6473 11.6239 15.2552 11.6239 14.7715C11.6239 14.2878 12.0161 13.8956 12.4998 13.8956Z" fill="white"></path>
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M11.631 1.70078C11.631 1.21768 12.0227 0.82605 12.5058 0.82605H15.9585C16.4416 0.82605 16.8333 1.21768 16.8333 1.70078C16.8333 2.18387 16.4416 2.5755 15.9585 2.5755H13.3805V9.35701C15.9909 9.77835 17.9845 12.0421 17.9845 14.7714C17.9845 17.8006 15.5289 20.2562 12.4998 20.2562C9.47065 20.2562 7.01505 17.8006 7.01505 14.7714C7.01505 12.0379 9.01473 9.77145 11.631 9.35509V1.70078ZM8.7645 14.7714C8.7645 12.7085 10.4368 11.0361 12.4998 11.0361C14.5627 11.0361 16.2351 12.7085 16.2351 14.7714C16.2351 16.8344 14.5627 18.5067 12.4998 18.5067C10.4368 18.5067 8.7645 16.8344 8.7645 14.7714Z" fill="white"></path>
